16c4fc1216a91518efe68c957f0965f839333bc1
sachinelavarasan <sachinelavarasan321@gmail.com>
2024-02-15 18:31:02 +0530

    auth bg image flicker issue fixes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,36 @@
 'use client';
 
-import Image from 'next/image';
 import React from 'react';
 
 import { useThemeData } from '../../utils/hooks/useThemeData';
+import { darkThemeData, lightThemeData } from '@/utils/theme-image';
 
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [currentTheme] = useThemeData();
-
   return (
-    <section className='h-screen'>
-      <div className='flex h-screen p-3' style={{backgroundImage:`url(${currentTheme?.authBgImg.src})`,  backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-       }}>
-        <div className='flex-1'>{children}</div>
+    <section className="h-screen">
+      <div
+        className="h-screen p-3 hidden dark:block"
+        style={{
+          backgroundImage: `url(${darkThemeData?.authBgImg.src})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>
+        <div className="h-full w-full">{children}</div>
+      </div>
+      <div
+        className="h-screen p-3 block dark:hidden"
+        style={{
+          backgroundImage: `url(${lightThemeData?.authBgImg.src})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>
+        <div className="h-full w-full">{children}</div>
       </div>
     </section>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning={true}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+              } else {
+                document.documentElement.classList.remove('dark')
+              }
+        `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-[#FFFFFF] dark:bg-[#0C1615]`}>
         <ThemeWrapper>{children}</ThemeWrapper>
       </body>
