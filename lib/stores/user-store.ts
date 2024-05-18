6585c0c2e2b9482d63d41824fd7d9e290c0bdc95
Janani811 <jananigopalsanthi@gmail.com>
2024-05-18 15:04:12 +0530

    login front end and api request

--- lib/stores/user-store.ts
+++ lib/stores/user-store.ts
@@ -1,25 +1,24 @@
 import { create } from 'zustand';
+
 import { UserI } from '../types';
 
 type State = {
-  count: number;
   currentUser: UserI | null;
+  error: string | null;
 };
 
 type Actions = {
-  increment: (qty: number) => void;
-  decrement: (qty: number) => void;
   setUser: (user: UserI) => void;
+  setError: (error: string) => void;
 };
 
 const initialState: State = {
-  count: 0,
   currentUser: null,
+  error: null,
 };
 
 export const useUserStore = create<State & Actions>((set) => ({
   ...initialState,
-  increment: (qty: number) => set((state) => ({ count: state.count + qty })),
-  decrement: (qty: number) => set((state) => ({ count: state.count - qty })),
   setUser: (user: UserI) => set((state) => ({ ...state, currentUser: user })),
+  setError: (error: string) => set((state) => ({ ...state, error })),
 }));
